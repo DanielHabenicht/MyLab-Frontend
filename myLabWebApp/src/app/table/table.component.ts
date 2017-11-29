e36ff3b04db08309d37ddb7b3247c93c4e9e3b19
Daniel Habenicht <Daniel.Habenicht@t-systems-mms.com>
2017-11-29 21:51:16 +0100

    Table complete

--- myLabWebApp/src/app/table/table.component.ts
+++ myLabWebApp/src/app/table/table.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Item } from '../common/classes/item.class';
+import { ITEMS } from '../mocks/items.mock';
 
 @Component({
-  selector: 'table',
+  selector: 'custom-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
 
-  //public items: string[] = ['Test 1', 'test2'];
+  public items: Item[];
+
+  constructor() {
+    //TODO Replace with real API
+    this.items = ITEMS;
+  }
 
 }
