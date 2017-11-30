9dd63fbd8914b21c77458f67cb6e70453c23da0e
Daniel Habenicht <Daniel.Habenicht@t-systems-mms.com>
2017-11-30 13:58:58 +0100

    Added Docker API

---
+++ myLabDockerAPI/myLabDockerAPI/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myLabAPI.Models
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Comment { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ myLabDockerAPI/myLabDockerAPI/Models/Typ.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myLabAPI.Models
+{
+    public class Type
+    {
+        public long Id { get; set; }
+        public string Attribute { get; set; }
+    }
+}
