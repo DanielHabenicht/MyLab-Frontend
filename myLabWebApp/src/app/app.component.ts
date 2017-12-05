39bb28c84b830b4bfb1c21247f845acb6d7f7eef
Daniel Habenicht <Daniel.Habenicht@t-systems-mms.com>
2017-12-06 00:30:34 +0100

    Added Barcode Scanner Reader

--- myLabWebApp/src/app/app.component.ts
+++ myLabWebApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { Message } from 'primeng/primeng';
+import { APIService } from './services/api.service';
+import { Device } from './common/classes/device.class';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,54 @@ import { Message } from 'primeng/primeng';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private readonly FILTER_DEBOUNCE_TIME = 100;
+  private searchFilter: number | null = null;
+
+  @HostListener('window:keyup', ['$event'])
+  public keyEvent(event: KeyboardEvent) {
+    const character = new RegExp('\\w');
+    if (event.key != null && character.test(event.key)) {
+      this.searchstring = this.searchstring.concat(event.key);
+    }
+    if (this.searchFilter != null) {
+      window.clearTimeout(this.searchFilter);
+    }
+    this.searchFilter = window.setTimeout(() => {
+      this.searchForBarcode();
+    }, this.FILTER_DEBOUNCE_TIME);
+  }
   public get location(): string {
     return this.router.url;
   }
 
+  public searchstring: string = '';
+
+  public devices: Device[] = new Array();
+
   public msgs: Message[] = [];
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private apiService: APIService
+
+  ) {
+    this.apiService.getDevices().subscribe(devices => {
+      this.devices = devices;
+    });
+
+  }
+
+  public searchForBarcode() {
+    const regex = new RegExp('^\\d{6}$');
+    if (this.searchstring === '') {
+      return;
+    }
+    if (!regex.test(this.searchstring)) {
+      this.searchstring = '';
+      return;
+    }
+    console.log(this.searchstring);
+    //this.router.navigate(['device/' + this.searchstring]);
 
   }
 }
